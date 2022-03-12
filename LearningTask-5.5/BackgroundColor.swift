82971cd603c723338f12ae6f6e47d9edf76e7241
Rafael Rollo <rafaelrollo92@gmail.com>
2022-03-12 17:04:00 +0000

    Adiciona estado base para o desafio

---
+++ LearningTask-5.5/BackgroundColor.swift
@@ -0,0 +1,12 @@
+
+import UIKit
+
+class BackgroundColor {
+    
+    static func para(_ bebida: DrinkGame.Bebida) -> UIColor {
+        return .white
+        
+        // UIColor(named:) pode ajudar a referenciar as cores do catálogo de Assets
+    }
+    
+}
